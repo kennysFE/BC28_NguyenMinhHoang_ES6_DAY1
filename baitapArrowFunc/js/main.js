f16bd0c3b729b21adbddc36f425660e7e3b02fef
hoangFE <metmoi556@email.com>
2022-07-29 22:44:30 +0700

    first commit

---
+++ baitapArrowFunc/js/main.js
@@ -0,0 +1,45 @@
+const colorList = [
+  "pallet",
+  "viridian",
+  "pewter",
+  "cerulean",
+  "vermillion",
+  "lavender",
+  "celadon",
+  "saffron",
+  "fuschia",
+  "cinnabar",
+];
+const renderButton = () => {
+  let html = "";
+  for (let i = 0; i < colorList.length; i++) {
+    let color = colorList[i];
+    let button = `<button onclick = "changeColor('${color}')" id = "colorButton" class = "color-button ${color}"></button>`;
+    html += button;
+  }
+  document.querySelector("#colorContainer").innerHTML = html;
+};
+
+window.changeColor = (color) => {
+  document.querySelector("#house").className = "house" + " " + color;
+};
+
+let onclickButton = () => {
+  let arrBtn = document.querySelectorAll("#colorContainer .color-button");
+  if (arrBtn) {
+    arrBtn.forEach(function (btn, key) {
+      btn.addEventListener("click", function () {
+        btn.classList.toggle("active");
+        arrBtn.forEach(function (a, b) {
+          if (key !== b) {
+            a.classList.remove("active");
+          }
+        });
+      });
+    });
+  }
+};
+window.onload = () => {
+  renderButton();
+  onclickButton()
+};
---
+++ baitapRest/js/main.js
@@ -0,0 +1,66 @@
+let tinhDiem = (...rest) => {
+  let sum = 0;
+  let count = 0;
+  for (let diem of rest) {
+    sum += diem;
+    count++;
+  }
+  return (sum / count).toFixed(2);
+};
+document.getElementById("btnKhoi1").onclick = function () {
+  diemToan = +document.getElementById("inpToan").value;
+  diemLy = +document.getElementById("inpLy").value;
+  diemHoa = +document.getElementById("inpHoa").value;
+  if (
+    diemToan !== parseInt(diemToan) ||
+    diemHoa !== parseInt(diemHoa) ||
+    diemLy !== parseInt(diemLy)
+  ) {
+    return alert("xin nhap diem");
+  }
+  if (!diemToan || !diemLy || !diemHoa) {
+    return alert("ko dc de trong");
+  }
+  if (diemToan < 0 || diemToan > 10) {
+    return alert("xin nhap lai");
+  }
+  if (diemLy < 0 || diemLy > 10) {
+    return alert("xin nhap lai");
+  }
+  if (diemHoa < 0 || diemHoa > 10) {
+    return alert("xin nhap lai");
+  }
+  sum = tinhDiem(diemHoa, diemToan, diemLy);
+  document.getElementById("tbKhoi1").innerHTML = sum;
+};
+document.getElementById("btnKhoi2").onclick = function () {
+  diemVan = +document.getElementById("inpVan").value;
+  diemSu = +document.getElementById("inpSu").value;
+  diemDia = +document.getElementById("inpDia").value;
+  diemEnglish = +document.getElementById("inpEnglish").value;
+  if (
+    diemVan !== parseInt(diemVan) ||
+    diemSu !== parseInt(diemSu) ||
+    diemDia !== parseInt(diemDia) ||
+    diemEnglish !== parseInt(diemEnglish)
+  ) {
+    return alert("xin nhap diem");
+  }
+  if (!diemVan || !diemSu || !diemDia || !diemEnglish) {
+    return alert("ko dc de trong");
+  }
+  if (diemVan < 0 || diemVan > 10) {
+    return alert("xin nhap lai");
+  }
+  if (diemSu < 0 || diemSu > 10) {
+    return alert("xin nhap lai");
+  }
+  if (diemDia < 0 || diemDia > 10) {
+    return alert("xin nhap lai");
+  }
+  if (diemEnglish < 0 || diemEnglish > 10) {
+    return alert("xin nhap lai");
+  }
+  sum = tinhDiem(diemDia, diemEnglish, diemSu, diemVan);
+  document.getElementById("tbKhoi2").innerHTML = sum;
+};
